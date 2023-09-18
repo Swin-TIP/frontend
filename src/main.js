4e32b0a1c6a83e788b75f576159c30d0d6055d78
Faqih <e0052804@u.nus.edu>
2023-09-18 18:17:26 +1000

    Create child routes for home component

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
+import Classes from './views/Classes.vue';
 import Home from './Home.vue';
 import Login from './views/Login.vue';
+import Schedule from './views/Schedule.vue';
 import Signup from './views/Signup.vue';
 
 import './assets/main.css';
 
 // Define routes
 const routes = [
-    { path: '/', component: Home },
+    {
+        path: '/', component: Home,
+        children: [
+            { path: '/classes', component: Classes },
+            { path: '/schedule', component: Schedule },
+        ]
+    },
     { path: '/login', component: Login },
     { path: '/signup', component: Signup }
 ];
