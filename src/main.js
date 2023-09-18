2929d3a43fe1ed92ed4680770d1a34e553a17181
Faqih <e0052804@u.nus.edu>
2023-09-18 15:45:21 +1000

    Create routes for login and signup pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
+import Home from './Home.vue';
+import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
 
-createApp(App).mount('#app')
+// Define routes
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/signup', component: Signup }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app');
---
+++ src/views/Signup.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    Sign Up Page
+</template>
