bb140624843034c93cc0a86be52cac1deb019d5b
Faqih <e0052804@u.nus.edu>
2023-10-04 23:11:03 +1100

    Create day selector component

---
+++ src/components/DaySelector.vue
@@ -0,0 +1,41 @@
+<script setup>
+</script>
+
+<template>
+    <div class="container">
+        <button class="tab">
+            M
+        </button>
+        <button class="tab">
+            T
+        </button>
+        <button class="tab">
+            W
+        </button>
+        <button class="tab">
+            T
+        </button>
+        <button class="tab">
+            F
+        </button>
+        <button class="tab">
+            S
+        </button>
+        <button class="tab">
+            S
+        </button>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    margin-left: 30px;
+}
+
+.tab {
+    height: 40px;
+    width: 70px;
+    border: 1px solid #B3B3B3;
+    cursor: pointer;
+}
+</style>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,10 +1,12 @@
 <script setup>
+import DaySelector from '../components/DaySelector.vue';
 import WeekSelector from '../components/WeekSelector.vue';
 </script>
 
 <template>
     <h1>Schedule</h1>
     <WeekSelector />
+    <DaySelector />
 </template>
 
 <style scoped></style>
