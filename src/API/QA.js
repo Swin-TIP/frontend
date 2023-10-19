0f8ff360cd31a35dde8ebde8553941d55738357a
Fai <laukimfairicky@gmail.com>
2023-10-19 13:00:58 +1100

    <UPDATE> Add Q&A query

--- src/API/QA.js
+++ src/API/QA.js
@@ -1,60 +1,72 @@
-import axios from "axios";
+import axios from 'axios'
 
-const API_URL = "https://swinburne-398109.ts.r.appspot.com/api";
+const API_URL = 'https://swinburne-398109.ts.r.appspot.com/api'
 
 export const postRequest = (inputText, token, sID) => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  }
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
 
-  const postData = {
-    question: inputText
-  }
+    const postData = {
+        question: inputText
+    }
 
-  return axios.post(`${API_URL}/qna/ask/${sID}`, postData, { headers })
+    return axios.post(`${API_URL}/qna/ask/${sID}`, postData, { headers })
 }
 
 export const getRequest = async (token, sID) => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  }
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
 
-  try {
-    const response = await axios.get(`${API_URL}/qna/list/${sID}`, { headers })
-    return response.data
-  } catch (error) {
-    throw error
-  }
+    try {
+        const response = await axios.get(`${API_URL}/qna/list/${sID}`, { headers })
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
+export const getSessionDetail = async (token, sID) => {
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
+    try {
+        const response = await axios.get(`${API_URL}/session/${sID}`, { headers })
+        return response.data
+    } catch (error) {
+        throw error
+    }
+}
 export const patchStatus = (mark, token, qID) => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  }
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+    }
 
-  const patchStatus = {
-    is_answered: mark
-  }
+    const patchStatus = {
+        is_answered: mark
+    }
 
-  return axios.patch(`${API_URL}/qna/answer/${qID}`, patchStatus, { headers })
+    return axios.patch(`${API_URL}/qna/answer/${qID}`, patchStatus, { headers })
 }
 
 export const patchVote = async (token, qID) => {
-  const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-    'question_id': `${qID}`
-  }
-  const body = {
-    "vote": true
-  }
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        question_id: `${qID}`
+    }
+    const body = {
+        vote: true
+    }
 
-  try {
-    return axios.patch(`${API_URL}/qna/vote/${qID}`, body, { headers })
-  } catch (error) {
-    throw error
-  }
+    try {
+        return axios.patch(`${API_URL}/qna/vote/${qID}`, body, { headers })
+    } catch (error) {
+        throw error
+    }
 }
