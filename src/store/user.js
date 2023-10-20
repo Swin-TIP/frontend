c6a0823ab665006fe6e24cf51948e3143431097a
FNasser1 <fn.alateeq@gmail.com>
2023-10-20 17:54:27 +1100

    Added pagination to the students page

--- src/store/user.js
+++ src/store/user.js
@@ -1,46 +1,46 @@
-import { reactive } from "vue";
+import { reactive } from 'vue'
 
 export const User = reactive({
   create(role, token) {
-    localStorage.setItem("token", token);
-    localStorage.setItem("role", role);
-    localStorage.setItem("approved", false);
-    localStorage.setItem("registeredClasses", JSON.stringify([]));
+    localStorage.setItem('token', token)
+    localStorage.setItem('role', role)
+    localStorage.setItem('approved', false)
+    localStorage.setItem('registeredClasses', JSON.stringify([]))
   },
   getApprovedStaus() {
-    return localStorage.approved;
+    return localStorage.approved
   },
   setApproved() {
-    localStorage.approved = true;
+    localStorage.approved = true
   },
   getRole() {
-    return localStorage.getItem("role");
+    return localStorage.getItem('role')
   },
   setRole(role) {
-    localStorage.setItem("role", role);
+    localStorage.setItem('role', role)
   },
   isAuthenticated() {
-    return !!localStorage.getItem("token");
+    return !!localStorage.getItem('token')
   },
   login(role, token) {
-    localStorage.setItem("token", token);
-    localStorage.setItem("role", role);
+    localStorage.setItem('token', token)
+    localStorage.setItem('role', role)
   },
   logout() {
-    localStorage.removeItem("token");
-    localStorage.removeItem("role");
-    localStorage.removeItem("approved");
-    localStorage.removeItem("registeredClasses");
+    localStorage.removeItem('token')
+    localStorage.removeItem('role')
+    localStorage.removeItem('approved')
+    localStorage.removeItem('registeredClasses')
   },
   getToken() {
-    return localStorage.token;
+    return localStorage.token
   },
   getRegisteredClasses() {
-    return JSON.parse(localStorage.registeredClasses);
+    return JSON.parse(localStorage.registeredClasses)
   },
   addRegisteredClass(classToRegister) {
-    const registeredClasses = JSON.parse(localStorage.registeredClasses);
-    registeredClasses = [...registeredClasses, classToRegister];
-    localStorage.registeredClasses = JSON.stringify(registeredClasses);
+    let registeredClasses = JSON.parse(localStorage.registeredClasses)
+    registeredClasses = [...registeredClasses, classToRegister]
+    localStorage.registeredClasses = JSON.stringify(registeredClasses)
   }
-});
+})
